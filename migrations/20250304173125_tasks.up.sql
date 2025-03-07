5166abc939f30982660956e5e438fbc49a9a8da2
Аркадий <51292927+arkad0912@users.noreply.github.com>
2025-03-07 10:06:36 +0300

    Delete migrations directory

--- migrations/20250304173125_tasks.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
-    task VARCHAR(255) NOT NULL,
-    is_done BOOLEAN DEFAULT FALSE,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    deleted_at TIMESTAMP DEFAULT NULL
-);
